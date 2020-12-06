5c19aef0f1adb64aadf317f189bab83901effde2
Usman Ahmad <engr.syedusmanahmad@gmail.com>
2020-12-06 14:15:02 +0500

    Update app.java
    Update hello world with lbistech

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello LbisTech!";
+    private final String message = "Hello World! from LbisTech!";
 
     public App() {}
 
